d3d6eba0f8ff1f3ac7299ed0226e6950d81df1b6
intrxx <michaloginski15@gmail.com>
2022-10-11 22:31:37 +0200

    Phaser setup

---
+++ js/PreloadGame.js
@@ -0,0 +1,19 @@
+import Phaser from "phaser";
+
+export default class PreloadGame extends Phaser.Scene 
+{
+    constructor()
+    {
+        super('PreloadGame')
+    }
+
+    preload()
+    {
+
+    }
+
+    create()
+    {
+        
+    }
+}
