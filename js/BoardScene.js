bf5da7c05705eb413c63b3f98399ec8f497b39f6
intrxx <michaloginski15@gmail.com>
2022-10-13 16:11:46 +0200

    Inported assets and added it to the scene

--- js/BoardScene.js
+++ js/BoardScene.js
@@ -1,3 +1,4 @@
+
 export default class BoardScene extends Phaser.Scene 
 {
     constructor()
@@ -5,8 +6,34 @@ export default class BoardScene extends Phaser.Scene
         super('BoardScene');
     }
 
+    preload()
+    {
+        //Loading Assets
+        //Boards
+        this.load.image('BlackBoard', '../assets/BlackBoard.png');
+        this.load.image('WhiteBoard', '../assets/WhiteBoard.png');
+
+        //Stones
+        this.load.image('WhiteStone1', '../assets/WhiteStone1.png');
+        this.load.image('WhiteStone2', '../assets/WhiteStone2.png');
+        this.load.image('BlackStone1', '../assets/BlackStone1.png');
+        this.load.image('BlackStone2', '../assets/BlackStone2.png');
+
+        //Others
+        this.load.image('Rope', '../assets/Rope.png');
+    }
+
     create() 
     {
+        //Boards
+        this.add.image(300, 245, 'BlackBoard').setScale(0.9);
+        this.add.image(900, 245, 'WhiteBoard').setScale(0.9);
+        this.add.image(300, 770, 'BlackBoard').setScale(0.9);
+        this.add.image(900, 770, 'WhiteBoard').setScale(0.9);
+
+        //Others
+        this.add.image(610,505, 'Rope');
+        
 
     }
 
--- js/main.js
+++ js/main.js
@@ -1,20 +1,15 @@
-import PreloadGame from './PreloadGame.js'
 import BoardScene from './BoardScene.js'
-
 
 const config = 
     {
     type: Phaser.AUTO,
     width: 1280,
-    height: 720,
+    height: 1080,
     backgroundColor: '#333333',
-    }
+    scene:  [BoardScene]
+    };
 
-    scene: 
-    [
-        PreloadGame,
-        BoardScene
-    ]
+    
 
 
 export default new Phaser.Game(config)
